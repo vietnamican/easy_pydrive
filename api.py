cfd4b67d2c239ebf8a53e1efb7931cf22887f97a
vietnamican <vietnamican@gmail.com>
2021-06-22 18:07:52 +0700

    upload a folder

--- api.py
+++ api.py
@@ -1,4 +1,5 @@
 import pydrive2
+import os
 
 def list_dir(drive, dir_id):
     query = {
@@ -16,14 +17,26 @@ def get_dir_reference(dir_path):
     d.FetchMetadata(fetch_all=True)
     return d
 
-def up_file(drive, file_path, dir_id):
+def up_file(drive, file_path, dir_id, is_folder=False):
+    base_name = os.path.basename(file_path)
     query = {
-        'parents': [{'id':dir_id}]
+        'parents': [{'id':dir_id}],
+        'title': base_name
     }
     f = drive.CreateFile(query)
-    f.SetContentFile('README.md')
+    f.SetContentFile(file_path)
     f.Upload()
+    return f
 
+def create_new_folder(drive, folder_title, dir_id):
+    query = {
+        'parents': [{'id':dir_id}],
+        'title': folder_title,
+        'mimeType': 'application/vnd.google-apps.folder'
+    }
+    f = drive.CreateFile(query)
+    f.Upload()
+    return f
 
 def up_files(drive, file_paths, dir_id):
     for file_path in file_paths:
@@ -31,3 +44,12 @@ def up_files(drive, file_paths, dir_id):
 
 def check_files_exist(drive, file_paths, dir_id):
     ...
+
+def up_folder(drive, folder_path, dir_id):
+    folder = create_new_folder(drive, folder_path, dir_id)
+    folder.FetchMetadata()
+    print(folder)
+    folder_id = folder.metadata['id']
+    file_paths = os.listdir(folder_path)
+    file_paths = [os.path.join(folder_path, file_path) for file_path in file_paths]
+    up_files(drive, file_paths, folder_id)
--- main.py
+++ main.py
@@ -1,16 +1,10 @@
 from time import sleep
 from auth import get_drive_instance
-from api import list_dir, up_files, check_files_exist
+from api import list_dir, up_files, check_files_exist, up_folder
 
 drive = get_drive_instance()
 
-
 parent_id = "1Bkoka3pDX60O3oqFKv0P85oiN1SmDpSZ"
 
-# up_files(drive, [1], parent_id)
-check_files_exist(drive, [1], parent_id)
-
-# file_list = list_dir(drive, parent_id)
-# print(file_list[0])
-# for file1 in file_list:
-#     print('title: %s, id: %s' % (file1['title'], file1['id']))
+# check_files_exist(drive, [1], parent_id)
+up_folder(drive, 'folder_a', parent_id)
