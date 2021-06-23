34af2bc9fa0d100f612637832114bd53a12431a6
vietnamican <vietnamican@gmail.com>
2021-06-23 14:10:36 +0700

    upload recursive

--- api.py
+++ api.py
@@ -20,22 +20,38 @@ def up_file(drive, file_path, dir_id, is_folder=False):
     f.Upload()
     return f
 
-def create_new_folder(drive, folder_title, dir_id):
+def create_empty_folder(drive, folder_title, dir_id):
+    base_name = os.path.basename(folder_title)
     query = {
         'parents': [{'id':dir_id}],
-        'title': folder_title,
+        'title': base_name,
         'mimeType': 'application/vnd.google-apps.folder'
     }
     f = drive.CreateFile(query)
-    exist = check_files_exist(drive, [folder_title], dir_id)[0]
-    if exist:
-        print("Existing a folder with the same name")
-        f = get_reference_by_name(drive, folder_title, dir_id)
-    else:
-        print("Creating a new folder")
-        f.Upload()
-        f.FetchMetadata()
+    f.Upload()
+    f.FetchMetadata()
+    exist = check_files_exist(drive, [base_name], dir_id)[0]
+    
     return f
+
+def up_items(drive, file_paths, dir_id):
+    items = []
+    for file_path in file_paths:
+        if os.path.isdir(file_path):
+            f = create_empty_folder(drive, file_path, dir_id)
+        else:
+            f = up_file(drive, file_path, dir_id)
+        items.append(f)
+    return items
+
+def check_files_exist(drive, file_paths, dir_id):
+    query = {
+        'supportsAllDrives': True,
+        'q': "'{}' in parents and trashed=false".format(dir_id)
+    }
+    file_list = drive.ListFile(query).GetList()
+    file_list_title = [f['title'] for f in file_list]
+    return [True if file_path in file_list_title else False for file_path in file_paths]
 
 def get_reference_by_name(drive, file_title, dir_id):
     exist = check_files_exist(drive, [file_title], dir_id)[0]
@@ -51,23 +67,45 @@ def get_reference_by_name(drive, file_title, dir_id):
             if f['title'] == file_title:
                 return f
 
-def up_files(drive, file_paths, dir_id):
-    for file_path in file_paths:
-        up_file(drive, file_path, dir_id)
-
-def check_files_exist(drive, file_paths, dir_id):
-    query = {
-        'supportsAllDrives': True,
-        'q': "'{}' in parents and trashed=false".format(dir_id)
-    }
-    file_list = drive.ListFile(query).GetList()
-    file_list_title = [f['title'] for f in file_list]
-    return [True if file_path in file_list_title else False for file_path in file_paths]
-
-def up_folder(drive, folder_path, dir_id):
-    folder = create_new_folder(drive, folder_path, dir_id)
-    # print(folder)
+def up_folder(drive, folder_path, dir_id, recursive=False):
+    folder_basename = os.path.basename(folder_path)
+    if check_files_exist(drive, [folder_basename], dir_id):
+        print("Existing a folder with the same name")
+        folder = get_reference_by_name(drive, folder_basename, dir_id)
+    else:
+        print("Creating a new folder")
+        folder = create_empty_folder(drive, folder_basename, dir_id)
     folder_id = folder.metadata['id']
-    file_paths = os.listdir(folder_path)
-    file_paths = [os.path.join(folder_path, file_path) for file_path in file_paths]
-    up_files(drive, file_paths, folder_id)
+
+    if recursive:
+        file_pathss = []
+        folder_ids = []
+        folder_paths = []
+        
+        folder_paths.append(folder_path)
+        folder_ids.append(folder_id)
+        while(True):
+
+            current_folder_path = folder_paths[0]
+            folder_paths = folder_paths[1:]
+            current_folder_id = folder_ids[0]
+            folder_ids = folder_ids[1:]
+
+            current_item_paths = os.listdir(current_folder_path)
+            current_item_paths = [os.path.join(current_folder_path, current_item_path) for current_item_path in current_item_paths]
+            current_file_paths = [current_item_path for current_item_path in current_item_paths if not os.path.isdir(current_item_path)]
+            current_folder_paths = [current_item_path for current_item_path in current_item_paths if os.path.isdir(current_item_path)]
+
+            up_items(drive, current_file_paths, current_folder_id)
+
+            if len(current_folder_paths) > 0:
+                current_folder_instances = up_items(drive, current_folder_paths, current_folder_id)
+                current_folder_ids = [instance.metadata['id'] for instance in current_folder_instances]
+                folder_ids.extend(current_folder_ids)
+                folder_paths.extend(current_folder_paths)
+            if len(folder_ids) == 0:
+                break
+    else:
+        file_paths = os.listdir(folder_path)
+        file_paths = [os.path.join(folder_path, file_path) for file_path in file_paths]
+        up_items(drive, file_paths, folder_id)
--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from time import sleep
 from auth import get_drive_instance
-from api import list_dir, up_files, check_files_exist, up_folder
+from api import list_dir, check_files_exist, up_folder
 
 drive = get_drive_instance()
 
 parent_id = "1Bkoka3pDX60O3oqFKv0P85oiN1SmDpSZ"
 
 # check_files_exist(drive, [1], parent_id)
-up_folder(drive, 'folder_a', parent_id)
+up_folder(drive, 'folder_a', parent_id, recursive=True)
